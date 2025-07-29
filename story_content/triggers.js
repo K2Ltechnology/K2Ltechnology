10abd4d640eaca2efcc84247a064dc676b58e5f3
K2Ltechnology <selva.son205@gmail.com>
2025-07-29 11:46:26 +0530

    29-07-2025

---
+++ story_content/triggers.js
@@ -0,0 +1,38 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "648GwNqCrIR":
+        Script1();
+        break;
+      case "5tQwKJA1WZv":
+        Script2();
+        break;
+      case "5nR2s8xBIuJ":
+        Script3();
+        break;
+      case "6jS777f4VAI":
+        Script4();
+        break;
+      case "6aGQ9rEicKj":
+        Script5();
+        break;
+  }
+}
+
+window.InitExecuteScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var once = player.once;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+var update = player.update;
+var pointerX = player.pointerX;
+var pointerY = player.pointerY;
+var showPointer = player.showPointer;
+var hidePointer = player.hidePointer;
+var slideWidth = player.slideWidth;
+var slideHeight = player.slideHeight;
+};
---
+++ story_content/user.js
@@ -0,0 +1,41 @@
+window.InitUserScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var once = player.once;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+var update = player.update;
+var pointerX = player.pointerX;
+var pointerY = player.pointerY;
+var showPointer = player.showPointer;
+var hidePointer = player.hidePointer;
+var slideWidth = player.slideWidth;
+var slideHeight = player.slideHeight;
+window.Script1 = function()
+{
+  getControls(1);
+}
+
+window.Script2 = function()
+{
+  getControls(1);
+}
+
+window.Script3 = function()
+{
+  getControls(1);
+}
+
+window.Script4 = function()
+{
+  getControls(1);
+}
+
+window.Script5 = function()
+{
+  getControls(1);
+}
+
+};
